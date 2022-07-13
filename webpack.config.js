3221b8226707e32bf7e869fdccabee45da142b4b
miss-gif <ddc.akari@gmail.com>
2022-07-14 05:39:00 +0900

    save

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+// import
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { VueLoaderPlugin } = require('vue-loader');
+
+// 내보내기 : JS -> export / node.js -> module.exports
+module.exports = {
+  resolve: {
+    extensions: ['.js','.vue']
+  },
+
+
+
+  // 파일을 읽어들이기 시작하는 진입점 설정
+  entry: "./src/main.js", // 자바스크립트를 진입점으로 한다.
+  // 결과물(번들)을 반환하는 설정
+  output: {
+    // path: path.resolve(__dirname, "dist"),
+    // 절대경로 표기, resolve메소드 1, 2경로를 합쳐준다.
+    // filename: "main.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          // 순서 중요!
+          'vue-style-loader',     
+
+
+
+          "style-loader",
+           "css-loader", 
+           "postcss-loader", 
+           "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
+  plugins: [
+    new HtmlPlugin({
+      template: "./index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "static" }],
+    }),
+    new VueLoaderPlugin(),
+  ],
+  devServer: {
+    host: "localhost",
+  },
+};
